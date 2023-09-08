b9a42ed827fbbb5e39de80db104911f7ba9af218
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2023-09-08 20:32:56 +0300

    update to 5.2

--- BrainFlowPlugin/Source/BrainFlowPlugin/BrainFlowPlugin.Build.cs
+++ BrainFlowPlugin/Source/BrainFlowPlugin/BrainFlowPlugin.Build.cs
@@ -5,8 +5,8 @@ using System.IO;
 
 public class BrainFlowPlugin : ModuleRules
 {
-	public BrainFlowPlugin(ReadOnlyTargetRules Target) : base(Target)
-	{
+    public BrainFlowPlugin(ReadOnlyTargetRules Target) : base(Target)
+    {
         PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
         PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
@@ -16,12 +16,15 @@ public class BrainFlowPlugin : ModuleRules
 
         if(Target.Platform == UnrealTargetPlatform.Win64)
         {
-            PublicLibraryPaths.Add(Path.Combine(ModuleDirectory, "Compiled", "x64_dynamic", "lib"));
             PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "Compiled", "x64_dynamic", "inc"));
-            PublicAdditionalLibraries.Add("Brainflow.lib");
-            PublicAdditionalLibraries.Add("DataHandler.lib");
-            PublicAdditionalLibraries.Add("BoardController.lib");
-            PublicAdditionalLibraries.Add("MLModule.lib");
+            
+            string Folder = Path.Combine(ModuleDirectory,  "Compiled", "macos", "lib");
+            PublicAdditionalLibraries.Add(Folder + "Brainflow.lib");
+            
+            PublicAdditionalLibraries.Add(Folder + "DataHandler.lib");
+            PublicAdditionalLibraries.Add(Folder + "BoardController.lib");
+            PublicAdditionalLibraries.Add(Folder + "MLModule.lib");
+            
             RuntimeDependencies.Add("$(TargetOutputDir)/BoardController.dll", Path.Combine(ModuleDirectory, "Compiled", "x64_dynamic", "lib", "BoardController.dll"));
             RuntimeDependencies.Add("$(TargetOutputDir)/DataHandler.dll", Path.Combine(ModuleDirectory, "Compiled", "x64_dynamic", "lib", "DataHandler.dll"));
             RuntimeDependencies.Add("$(TargetOutputDir)/GanglionLib.dll", Path.Combine(ModuleDirectory, "Compiled", "x64_dynamic", "lib", "GanglionLib.dll"));
@@ -35,11 +38,14 @@ public class BrainFlowPlugin : ModuleRules
         if(Target.Platform == UnrealTargetPlatform.Mac)
         {
             PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "Compiled", "macos", "inc"));
-            PublicLibraryPaths.Add(Path.Combine(ModuleDirectory, "Compiled", "macos", "lib"));
-            PublicAdditionalLibraries.Add("libBrainflow.a");
-            PublicDelayLoadDLLs.Add("libDataHandler.dylib");
-            PublicDelayLoadDLLs.Add("libBoardController.dylib");
-            PublicDelayLoadDLLs.Add("libMLModule.dylib");
+
+            string Folder = Path.Combine(ModuleDirectory,  "Compiled", "macos", "lib");
+            PublicAdditionalLibraries.Add(Folder + "/libBrainflow.a");
+            
+            PublicDelayLoadDLLs.Add(Folder + "/libDataHandler.dylib");
+            PublicDelayLoadDLLs.Add(Folder + "/libBoardController.dylib");
+            PublicDelayLoadDLLs.Add(Folder + "/libMLModule.dylib");
+
             RuntimeDependencies.Add("$(TargetOutputDir)/libBoardController.dylib", Path.Combine(ModuleDirectory, "Compiled", "macos", "lib", "libBoardController.dylib"));
             RuntimeDependencies.Add("$(TargetOutputDir)/libDataHandler.dylib", Path.Combine(ModuleDirectory, "Compiled", "macos", "lib", "libDataHandler.dylib"));
             RuntimeDependencies.Add("$(TargetOutputDir)/libGanglionLib.dylib", Path.Combine(ModuleDirectory, "Compiled", "macos", "lib", "libGanglionLib.dylib"));
@@ -50,11 +56,14 @@ public class BrainFlowPlugin : ModuleRules
         if(Target.Platform == UnrealTargetPlatform.Linux)
         {
             PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "Compiled", "linux", "inc"));
-            PublicLibraryPaths.Add(Path.Combine(ModuleDirectory, "Compiled", "linux", "lib"));
-            PublicAdditionalLibraries.Add("libBrainflow.a");
-            PublicDelayLoadDLLs.Add("libDataHandler.so");
-            PublicDelayLoadDLLs.Add("libBoardController.so");
-            PublicDelayLoadDLLs.Add("libMLModule.so");
+            
+            string Folder = Path.Combine(ModuleDirectory,  "Compiled", "linux", "lib");
+            PublicAdditionalLibraries.Add(Folder + "/libBrainflow.a");
+            
+            PublicDelayLoadDLLs.Add(Folder + "/libDataHandler.so");
+            PublicDelayLoadDLLs.Add(Folder + "/libBoardController.so");
+            PublicDelayLoadDLLs.Add(Folder + "/libMLModule.so");
+            
             RuntimeDependencies.Add("$(TargetOutputDir)/libBoardController.so", Path.Combine(ModuleDirectory, "Compiled", "linux", "lib", "libBoardController.so"));
             RuntimeDependencies.Add("$(TargetOutputDir)/libDataHandler.so", Path.Combine(ModuleDirectory, "Compiled", "linux", "lib", "libDataHandler.so"));
             RuntimeDependencies.Add("$(TargetOutputDir)/libGanglionLib.so", Path.Combine(ModuleDirectory, "Compiled", "linux", "lib", "libGanglionLib.so"));
